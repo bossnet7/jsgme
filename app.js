a166f2cab79fb9a48c8e5743c3f4fc40449eef5c
bossnet <bossnet@web.de>
2017-01-22 01:43:56 +0100

    daten werden an nodejs server gesendet, antwort wird verarbeitet -> node schickt fake daten um player zu spawnen

--- app.js
+++ app.js
@@ -1,16 +1,37 @@
-var
-	data = {
-		players : [
-			{
-				name : 0
-				id : 0
-				top : 0
-				left : 0
-				right : 0
-				bottom : 0
-				points : 0
-			}
-		]
-	}
+var	
+  http = require('http')
+, testJSON = {
+		"spieler2" : {
+			"name" : "spieler2",
+			"location" : {
+				"top" : 182, 
+				"left" : 187,
+				"right" : 768,
+				"bottom" : 101,
+				"width" : 40,
+				"height" : 40
+			},
+			"direction" : "right" 
+		}
+	}  
 ;
 
+const PORT = 2509;
+server = http.createServer(onRequest).listen(2509);
+console.log("server is running !");
+
+function onRequest(req, res) {
+	var 
+	  response = JSON.stringify(testJSON);
+	;
+  res.writeHead(200, {
+      'Content-Type': 'application/json'
+    , 'Access-Control-Allow-Origin': '*'
+    , 'Access-Control-Allow-Methods': 'POST,GET,OPTIONS'
+    , 'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
+  });
+	res.write(response, function(err) {
+		console.log("sending")
+		res.end();
+	});
+}
